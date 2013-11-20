9a2e49811c1f6e2fd5b646f73b68c7c299941f2d
Paul Jones <paulmmj@gmail.com>
2013-11-20 13:13:56 -0500

    a what happened method and more sample code

--- Drink.io/Drink.io/RUDBManager.m
+++ Drink.io/Drink.io/RUDBManager.m
@@ -54,18 +54,13 @@ withSubAdministrativeArea: (NSString *) subAdministrativeArea
     
     if (sqlite3_open(dbpath, &database) == SQLITE_OK)
     {
-        NSString *insertSQL = [NSString stringWithFormat:@"insert into bars"
-                               "(bar,phoneNumber,URL,throughfare,subThroughfare,locality,subLocality,"
-                               "administrativeArea, subAdministrativeArea, postalCode, ISOcountryCode,"
-                               "country, favorite)"
-                               "values (\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%d\")",
-                               bar, phoneNumber, URL, thoroughfare, subThoroughfare, locality, subLocality, administrativeArea,
-                               subAdministrativeArea, postalCode, ISOcountryCode, country, favorite];
+        NSString *insertSQL = [NSString stringWithFormat:@"insert into sells (bar, beer, price) values (\"Clydez\",\"Bud\",20)"];
         
         NSLog(@"%@", insertSQL);
         
         const char *insert_stmt = [insertSQL UTF8String];
         sqlite3_prepare_v2(database, insert_stmt,-1, &statement, NULL);
+        [self whatHappened];
         if (sqlite3_step(statement) == SQLITE_DONE)
         {
             return YES;
@@ -76,6 +71,150 @@ withSubAdministrativeArea: (NSString *) subAdministrativeArea
         sqlite3_reset(statement);
     }
     return NO;
+}
+
+- (int)insertIdentifier:(NSString *)identifier
+               lastName:(NSString *)lastName
+              firstName:(NSString *)firstName
+                  email:(NSString *)email
+{
+    int result;
+    
+    const char *insert_stmt = "INSERT INTO UsersTable (id, lastName, firstName, email) VALUES (?, ?, ?, ?);";
+    
+    if ((result = sqlite3_prepare_v2(database, insert_stmt, -1, &statement, NULL)) != SQLITE_OK)
+    {
+        NSLog(@"%s: prepare failure '%s' (%d)", __FUNCTION__, sqlite3_errmsg(database), result);
+        return result;
+    }
+    
+    if ((result = sqlite3_bind_text(statement, 1, [identifier UTF8String], -1, NULL)) != SQLITE_OK)
+    {
+        NSLog(@"%s: bind #1 failure '%s' (%d)", __FUNCTION__, sqlite3_errmsg(database), result);
+        sqlite3_finalize(statement);
+        return result;
+    }
+    
+    if ((result = sqlite3_bind_text(statement, 2, [lastName UTF8String], -1, NULL)) != SQLITE_OK)
+    {
+        NSLog(@"%s: bind #2 failure '%s' (%d)", __FUNCTION__, sqlite3_errmsg(database), result);
+        sqlite3_finalize(statement);
+        return result;
+    }
+    
+    if ((result = sqlite3_bind_text(statement, 3, [firstName UTF8String], -1, NULL)) != SQLITE_OK)
+    {
+        NSLog(@"%s: bind #3 failure '%s' (%d)", __FUNCTION__, sqlite3_errmsg(database), result);
+        sqlite3_finalize(statement);
+        return result;
+    }
+    
+    if ((result = sqlite3_bind_text(statement, 4, [email UTF8String], -1, NULL)) != SQLITE_OK)
+    {
+        NSLog(@"%s: bind #4 failure '%s' (%d)", __FUNCTION__, sqlite3_errmsg(database), result);
+        sqlite3_finalize(statement);
+        return result;
+    }
+    
+    if ((result = sqlite3_step(statement)) != SQLITE_DONE)
+    {
+        NSLog(@"%s: step failure: '%s'", __FUNCTION__, sqlite3_errmsg(database));
+    }
+    
+    sqlite3_finalize(statement);
+    
+    return result;
+}
+
+- (void) whatHappened {
+    
+    if (sqlite3_step(statement) == SQLITE_OK) {
+        NSLog(@"Ok");
+    }
+    else if (sqlite3_step(statement) == SQLITE_ERROR) {
+        NSLog(@"Error");
+    }
+    else if (sqlite3_step(statement) == SQLITE_INTERNAL) {
+        NSLog(@"Internal");
+    }
+    else if (sqlite3_step(statement) == SQLITE_PERM) {
+        NSLog(@"Perm");
+    }
+    else if (sqlite3_step(statement) == SQLITE_ABORT) {
+        NSLog(@"Abort");
+    }
+    else if (sqlite3_step(statement) == SQLITE_BUSY) {
+        NSLog(@"Busy");
+    }
+    else if (sqlite3_step(statement) == SQLITE_LOCKED) {
+        NSLog(@"Locked");
+    }
+    else if (sqlite3_step(statement) == SQLITE_NOMEM) {
+        NSLog(@"Nomem");
+    }
+    else if (sqlite3_step(statement) == SQLITE_READONLY) {
+        NSLog(@"Readonly");
+    }
+    else if (sqlite3_step(statement) == SQLITE_INTERRUPT) {
+        NSLog(@"Interrupt");
+    }
+    else if (sqlite3_step(statement) == SQLITE_IOERR) {
+        NSLog(@"IOErr");
+    }
+    else if (sqlite3_step(statement) == SQLITE_CORRUPT) {
+        NSLog(@"Corrupt");
+    }
+    else if (sqlite3_step(statement) == SQLITE_NOTFOUND) {
+        NSLog(@"Not found");
+    }
+    else if (sqlite3_step(statement) == SQLITE_FULL) {
+        NSLog(@"Full");
+    }
+    else if (sqlite3_step(statement) == SQLITE_CANTOPEN) {
+        NSLog(@"Can't open");
+    }
+    else if (sqlite3_step(statement) == SQLITE_PROTOCOL) {
+        NSLog(@"Potocol");
+    }
+    else if (sqlite3_step(statement) == SQLITE_EMPTY) {
+        NSLog(@"Empty");
+    }
+    else if (sqlite3_step(statement) == SQLITE_SCHEMA) {
+        NSLog(@"Schema");
+    }
+    else if (sqlite3_step(statement) == SQLITE_TOOBIG) {
+        NSLog(@"Too big");
+    }
+    else if (sqlite3_step(statement) == SQLITE_CONSTRAINT) {
+        NSLog(@"Constraint");
+    }
+    else if (sqlite3_step(statement) == SQLITE_MISMATCH) {
+        NSLog(@"Mismatch");
+    }
+    else if (sqlite3_step(statement) == SQLITE_MISUSE) {
+        NSLog(@"Misuse");
+    }
+    else if (sqlite3_step(statement) == SQLITE_NOLFS) {
+        NSLog(@"Nolfs");
+    }
+    else if (sqlite3_step(statement) == SQLITE_AUTH) {
+        NSLog(@"Auth");
+    }
+    else if (sqlite3_step(statement) == SQLITE_FORMAT) {
+        NSLog(@"Format");
+    }
+    else if (sqlite3_step(statement) == SQLITE_RANGE) {
+        NSLog(@"Range");
+    }
+    else if (sqlite3_step(statement) == SQLITE_NOTADB) {
+        NSLog(@"Not a DB");
+    }
+    else if (sqlite3_step(statement) == SQLITE_ROW) {
+        NSLog(@"Row");
+    }
+    else if (sqlite3_step(statement) == SQLITE_DONE) {
+        NSLog(@"Done");
+    }
 }
 
 - (BOOL) makeBestFriendWithFirstName: (NSString *) firstName
